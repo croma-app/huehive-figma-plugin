f2d87a3a8016cc235025f81f5fb52f4922fa6c38
bhuwan joshi <bhuwanchandra.it@gmail.com>
2023-08-20 18:46:58 +0530

    Simple header css added

--- src/app/components/common/Header/index.tsx
+++ src/app/components/common/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UserInfo } from '../../../types';
+import './index.css';
 
 interface HeaderProps {
   userInfo: UserInfo;
@@ -8,35 +9,31 @@ interface HeaderProps {
 const Header = function (props: HeaderProps) {
   const { userInfo } = props;
   return (
-    <div>
-      <div className="header-branding-area navbar-brand">
+    <header className="header">
+      <div className="header-name">
         <div className="logo">
           <img src="https://huehive.co/assets/logos/croma-fdaef4e29c5ef06a91d4f4272650e9b5077985204aaa20dea78173eeb360ef80.svg" />
         </div>
-        <div className="header-name">
-          <h2>Huehive</h2>
-        </div>
+        <h2>Huehive</h2>
       </div>
-      <div>
-        <div className="kebab">
-          <img src={userInfo.user.avatar_url} height={60} />
-          <ul className="dropdown">
-            <li>
-              <a href="http://www.g.com">Art</a>
-            </li>
-            <li>
-              <a href="http://www.g.com">Coding</a>
-            </li>
-            <li>
-              <a href="http://www.g.com">Design</a>
-            </li>
-            <li>
-              <a href="http://www.g.com">Web Development</a>
-            </li>
-          </ul>
-        </div>
+      <div className="kebab">
+        <img className="avatar" src={userInfo.user.avatar_url} height={60} />
+        <ul className="dropdown">
+          <li>
+            <a href="http://www.g.com">Art</a>
+          </li>
+          <li>
+            <a href="http://www.g.com">Coding</a>
+          </li>
+          <li>
+            <a href="http://www.g.com">Design</a>
+          </li>
+          <li>
+            <a href="http://www.g.com">Web Development</a>
+          </li>
+        </ul>
       </div>
-    </div>
+    </header>
   );
 };
 
